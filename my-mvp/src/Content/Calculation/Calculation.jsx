bab977d35b62ea45b8a3354e670d11468db62dfb
SVLakhtarev <svlakhtarev@gmail.com>
2021-09-09 15:57:08 +0300

    0909

--- my-mvp/src/Content/Calculation/Calculation.jsx
+++ my-mvp/src/Content/Calculation/Calculation.jsx
@@ -31,10 +31,11 @@ export class Calculation extends Component {
         </Col>
       </Row>
       <Row justify='center' gutter={{xs: 8, sm: 16, md: 24, lg: 32}}>
+        {/*Обернуть ещё и фильтрацией всего массива Фильтр, для того, чтобы фильтровать по value с typeID. По нажатию кнопки, отдать отфильтрованные данные в useState, чтобы при нажатии на результат, выдать результат*/}
         {filter.map(item => (
           <Col className="gutter-row" span={6}>
             <Space direction="vertical" key={item.categoryID}>
-              <Card title={item.lable} style={{width: 300}}>
+              <Card title={item.lable} className={style.card}>
                 <Select defaultValue={item.options[0].title} onChange={onChange}>
                   {item.options.map(option => (
                     <option key={option.typeID}
@@ -51,7 +52,7 @@ export class Calculation extends Component {
       </Row>
       <Button className={style.button} onClick={this.resultStateHandler}>Result</Button>
       {this.state.resultCallState ? <Result/> : null}
-      {/*как сделать так, чтобы кнопка была не активна (disabled??), если хоть один Select value = null ?*/}
+      {/*как сделать так, чтобы кнопка была не активна (disabled??), если хоть один Select value = null ? Обращение к тому, что лежит в State?*/}
     </div>
   }
 }
--- my-mvp/src/Content/Calculation/Result/ResultSuppliers.jsx
+++ my-mvp/src/Content/Calculation/Result/ResultSuppliers.jsx
@@ -1,6 +1,7 @@
 import React, {Component} from 'react'
 import {suppliers} from './suppliers'
 import {Card, Col, Row, Space} from 'antd'
+import style from './../Calculation.module.css'
 
 export class ResultSuppliers extends Component {
 
@@ -15,16 +16,16 @@ export class ResultSuppliers extends Component {
         {suppliers.map(item => (
           <Row gutter={[16, 24]}>
             <Col className="gutter-row" span={24}>
-              <h3>
+              <h3 className={style.h3}>
                 <hr/>
                 {item.lable}
                 <hr/>
               </h3>
             </Col>
             {item.options.map(option => (
-              <Col span={12}>
+              <Col span={8}>
                 <Space direction="vertical">
-                  <Card title={option.marketTitle} style={{width: 300}}>
+                  <Card title={option.marketTitle} className={style.card}>
                     <div><strong>Location: </strong>{option.location}</div>
                     <div><strong>Phone: </strong>{option.phone}</div>
                     <div><strong>E-Mail: </strong>{option.email}</div>
